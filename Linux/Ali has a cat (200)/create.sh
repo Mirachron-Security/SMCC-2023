ccc24de5a0ca6cef137d7a267fcce45ab1cc21fd
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2022-12-20 18:41:18 +0100

    Create create.sh

---
+++ Linux/Ali has a cat (200)/create.sh
@@ -0,0 +1,28 @@
+# user
+useradd -m user -s /bin/bash
+echo "user:userpass" | chpasswd
+
+# flag
+echo "flag{who_let_the_cat_out?}" > /home/user/flag.txt
+
+# history
+ln -sf /dev/null ~/.bash_history
+
+# aliases
+echo alias cat=/bin/true >> /home/user/.bashrc
+echo alias less=/bin/true >> /home/user/.bashrc
+echo alias more=/bin/true >> /home/user/.bashrc
+echo alias find=/bin/true >> /home/user/.bashrc
+echo alias grep=/bin/true >> /home/user/.bashrc
+echo alias egrep=/bin/true >> /home/user/.bashrc
+echo alias vi=/bin/true >> /home/user/.bashrc
+echo alias vim=/bin/true >> /home/user/.bashrc
+echo alias fgrep=/bin/true >> /home/user/.bashrc
+echo alias nano=/bin/true >> /home/user/.bashrc
+echo alias python=/bin/true >> /home/user/.bashrc
+echo alias python2=/bin/true >> /home/user/.bashrc
+echo alias python3=/bin/true >> /home/user/.bashrc
+echo alias source=/bin/true >> /home/user/.bashrc
+echo alias who=/bin/true >> /home/user/.bashrc
+echo alias last=/bin/true >> /home/user/.bashrc
+echo alias w=/bin/true >> /home/user/.bashrc
