779a0ae99846e8db2ac841505230f14ea567b732
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2022-12-21 16:19:34 +0100

    Create get-flag.py

---
+++ .Preparation/Linux1/get-flag.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3 
+
+from pwn import *
+import re
+
+context.log_level = "critical"
+host = "206.189.59.199"
+port = 9999
+s = remote(host,port)
+
+# receive info from server
+text = s.recv().decode()
+#print(text)
+
+# send answer
+s.sendline(b"ianuarie")
+
+# get response and trim out the flag
+response = s.recvline().decode()
+flag = response.split(" ")[5]
+
+print(flag)
+
+# close connection
+s.close()
