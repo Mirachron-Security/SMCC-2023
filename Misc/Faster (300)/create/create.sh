0ff7fafd559fa89e0661e6e73c1b773e60d43585
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-03-03 14:08:44 +0200

    Update create.sh

--- Misc/Faster (300)/create/create.sh
+++ Misc/Faster (300)/create/create.sh
@@ -2,5 +2,5 @@
 
 # simultaneous connections from multiple users 
 # create a pty because the python script accepts input from a terminal
+# redirect errors to /dev/null !!
 /usr/bin/socat tcp-listen:1337,fork system:'exec /usr/bin/python3 /home/chronos/challenges/misc/faster/hosting.py 2>/dev/null',pty,raw,echo=0
-
