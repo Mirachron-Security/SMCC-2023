24cc5974c61441f6663f77b2332184c22e5b813b
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-03-03 14:12:12 +0200

    Update hosting.py

--- .Preparation/Linux1/create/hosting.py
+++ .Preparation/Linux1/create/hosting.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 
-from pwn import *
 import re
 from inputimeout import inputimeout, TimeoutOccurred
+import sys
+from time import sleep
+
 
 # user interation
 print("Salut! Concursul se apropie!")
+#sleep(0.5)
 print("Veti mai primi cateva exercitii pentru acomodare, sper sa va placa.")
+#sleep(0.5)
 print("""Pentru inceput, raspunde la aceasta intrebare (ai doar 3 secunde): 
 
-In luna va avea loc concursul?
+In ce luna va avea loc concursul?
 """)
 
 # correct response
 correct = "martie"
 
 # read the flag from local machine
-f_open = open("flag.txt","r")
+f_open = open("/home/chronos/challenges/pre-ctf/give-me-the-date/flag.txt","r")
 f = f_open.read()
 
 try:
    # input gets only 3 seconds before timeout
-   response= inputimeout(prompt='>> ', timeout=5)
+   response= inputimeout(prompt='>> ', timeout=3)
 
    # compare solution with input
    if (re.match(correct,response.lower())):
@@ -34,3 +38,7 @@ try:
 except TimeoutOccurred:
    print("Prea lent!")
    exit()
+# in case the user exists unexpectedly
+except OSError:
+    print('Input/output error occurred. Exiting.')
+    sys.exit(1)
