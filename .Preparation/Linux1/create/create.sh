5258bc279f1f01f21451e47a30423443c3e70a3a
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-03-03 14:19:42 +0200

    Update create.sh

--- .Preparation/Linux1/create/create.sh
+++ .Preparation/Linux1/create/create.sh
@@ -2,4 +2,5 @@
 
 # simultaneous connections from multiple users 
 # create a pty because the python script accepts input from a terminal
-/usr/bin/socat tcp-listen:9999,fork system:'exec /usr/bin/python3 /home/chronos/challenges/pre-ctf/give-me-the-date/hosting.py',pty,raw,echo=0
+# redirect errors to /dev/null !!
+/usr/bin/socat tcp-listen:9999,fork system:'exec /usr/bin/python3 /home/chronos/challenges/pre-ctf/give-me-the-date/hosting.py 2>/dev/null',pty,raw,echo=0
