82a30ec5532b877ed4fa732135c8e2b5aa6df01c
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-03-03 22:48:02 +0200

    Update kill_scripts.sh

--- .Infrastructure/manage_script_processes/kill_scripts.sh
+++ .Infrastructure/manage_script_processes/kill_scripts.sh
@@ -2,9 +2,9 @@
 
 # Define an array of script names to check
 SCRIPTS=(
-    "/home/chronos/CTFs/AFT/discord/discord/discord-bot-creds.py" 
-    
-    )
+    "/home/chronos/discord/discord-creds-bot.py"
+    "/home/chronos/forensics/sniff_away/verify_srv.py"
+)
 
 # Loop over the script names and find the process IDs of each script
 for script_path in "${SCRIPTS[@]}"
@@ -12,13 +12,19 @@ do
     script=$(basename "$script_path")
     # Use pgrep to find the process ID(s) of the script
     pids=$(pgrep -f "$script")
-    
-    # Loop over the process IDs and kill each one
-    for pid in $pids
-    do
-        if [[ $pid != $$ ]]; then # skip the current process running the script
-            echo "Killing process with ID $pid (script: $script)..."
-            kill $pid
-        fi
-    done
+
+    if [ -z "$pids" ]; then
+        echo "Script $script is not running."
+    else
+        echo "Script $script is running with PID(s): $pids"
+        # Loop over the process IDs and kill each one
+        for pid in $pids
+        do
+            if [[ $pid != $$ ]]; then # skip the current process running the script
+                echo "Killing process with ID $pid (script: $script)..."
+                kill $pid
+            fi
+        done
+        echo ""
+    fi
 done
