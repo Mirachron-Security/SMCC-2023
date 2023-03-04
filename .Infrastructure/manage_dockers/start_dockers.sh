0274b5b1d2df2b44ff53aabf57b4f2af3aed5c36
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-03-04 23:59:32 +0200

    Create start_dockers.sh

---
+++ .Infrastructure/manage_dockers/start_dockers.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+for i in `find /home/chronos -name runme.sh -exec realpath {} \;`; do 
+    $i 2>/dev/null;
+done
+
+# -_-
+docker-compose -f /home/chronos/challenges/web/web-waffle-8004/docker-compose.yaml up -d
