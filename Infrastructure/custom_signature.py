33786e337281aa80992633e1b87508149d027519
ChronosPK <93029180+ChronosPK@users.noreply.github.com>
2023-09-23 12:33:46 +0300

    Update custom_signature.py

--- Infrastructure/custom_signature.py
+++ Infrastructure/custom_signature.py
@@ -7,20 +7,7 @@
 #|##############################|#
 
 import os
-
-# Update this path to the desired directory
-path = "." 
-banner = """
-
-#|##############################|#
-#| Chronos Security             |#
-#| https://chronossec.site      |#
-#| https://github.com/ChronosPK |#
-#|##############################|#
-
-"""
-python_sign = "#!/usr/bin/python3" + banner
-bash_sign = "#!/bin/bash" + banner
+import fnmatch
 
 # Colored output
 R = "\033[0;31m"  # Red
@@ -28,34 +15,62 @@ G = "\033[0;32m"  # Green
 B = "\033[0;34m"  # Blue
 NO = "\033[0m"    # No color
 
-def process_file(file_path):
+# Define the shebangs and banner message
+python_shebang = "#!/usr/bin/python3"
+bash_shebang = "#!/bin/bash"
+
+# Define the new banner without color
+banner = """
+#|##############################|#
+#| Chronos Security             |#
+#| https://chronossec.site      |#
+#| https://github.com/ChronosPK |#
+#|##############################|#
+
+"""
+
+# Check if the banner is present
+def has_banner(file_content):
+    return banner.strip() in file_content.strip()
+
+# Add the banner to file
+def add_banner_to_file(file_path):
     with open(file_path, 'r') as f:
         content = f.read()
 
-    is_modified = False
+    if has_banner(content):
+        return False
 
-    if python_sign not in content and bash_sign not in content:
-        if file_path.endswith('.sh'):
-            content = bash_sign + content
+    with open(file_path, 'w') as f:
+        if content.startswith(python_shebang) or content.startswith(bash_shebang):
+            shebang, rest = content.split('\n', 1)
+            f.write(shebang + '\n' + banner + rest)
         else:
-            content = python_sign + content
-        is_modified = True
-
-        with open(file_path, 'w') as f:
+            f.write(f"{python_shebang}\n" if file_path.endswith('.py') else f"{bash_shebang}\n")
+            f.write(banner)
             f.write(content)
 
-    return is_modified
+    return True
 
-def process_directory(directory):
+# Process files recursively
+def process_files(directory):
+    modified_count = 0
+    unmodified_count = 0
+    
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith(('.py', '.sh')):
+            if fnmatch.fnmatch(file, '*.sh') or fnmatch.fnmatch(file, '*.py'):
                 file_path = os.path.join(root, file)
-                is_modified = process_file(file_path)
-                if is_modified:
+                if add_banner_to_file(file_path):
                     print(f"{B}Modified:{NO} {file_path}")
+                    modified_count += 1
                 else:
-                    print(f"{G}Unchanged:{NO} {file_path}")
+                    print(f"{G}Unmodified:{NO} {file_path}")
+                    unmodified_count += 1
+
+    print(f"\n{B}Modified files: {modified_count}{NO}")
+    print(f"{G}Unmodified files: {unmodified_count}{NO}")
 
 if __name__ == "__main__":
-    process_directory(path)
+    directory = input("Enter the directory path: ")
+    process_files(directory)
